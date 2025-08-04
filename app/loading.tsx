2c133d7c63c4c82d0bc44ea71a06cc077216a405
Vadzim Simanau <eks220885@gmail.com>
2025-08-04 22:59:42 +0300

    add file loading tsx, connect it with Loader

---
+++ app/loading.tsx
@@ -0,0 +1,6 @@
+import Loader from "@/components/Loader/Loader";
+
+// Цей файл автоматично відображається Next.js під час завантаження даних для будь-якого маршруту
+export default function Loading() {
+  return <Loader />;
+}
