8cfe6a7135c955b3131c45e42c80d0de552e154f
Vadzim Simanau <eks220885@gmail.com>
2025-08-04 23:04:57 +0300

    correct footer contacts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,10 +7,10 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: your name</p>
+          <p>Developer: Vadzim Simanau</p>
           <p>
             Contact us:
-            <a href="mailto:student@notehub.app">student@notehub.app</a>
+            <a href="mailto:eks220885@gmail.com"> eks220885@gmail.com</a>
           </p>
         </div>
       </div>
