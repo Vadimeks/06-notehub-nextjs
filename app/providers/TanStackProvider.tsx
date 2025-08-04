e1978955fb740bfa9f0a046872fefba88b1ae599
Vadzim Simanau <eks220885@gmail.com>
2025-08-04 23:39:25 +0300

    change ReactQueryProvider to TanStackProvider

---
+++ app/providers/TanStackProvider.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  DehydratedState,
+  HydrationBoundary,
+} from "@tanstack/react-query";
+import React, { useState } from "react";
+
+interface ReactQueryProviderProps {
+  children: React.ReactNode;
+  dehydratedState?: DehydratedState;
+}
+
+export default function ReactQueryProvider({
+  children,
+  dehydratedState,
+}: ReactQueryProviderProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* Замест Hydrate выкарыстоўваем HydrationBoundary */}
+      <HydrationBoundary state={dehydratedState}>{children}</HydrationBoundary>
+    </QueryClientProvider>
+  );
+}
