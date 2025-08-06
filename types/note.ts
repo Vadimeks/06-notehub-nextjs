512da6aa201052ee2c5f5eead0ddcd380df6c12c
Vadzim Simanau <eks220885@gmail.com>
2025-08-06 12:18:33 +0300

    remove FetchNotesResponse from note.ts

--- types/note.ts
+++ types/note.ts
@@ -6,8 +6,3 @@ export interface Note {
   updatedAt: string;
   tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
 }
-
-export interface FetchNotesResponse {
-  notes: Note[];
-  totalPages: number;
-}
