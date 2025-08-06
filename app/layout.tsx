f409d815e2d54d93b0212acc1c3cff0acaf7e1a7
Vadzim Simanau <eks220885@gmail.com>
2025-08-06 12:24:56 +0300

    include Header and footer into TanStackProvider - file layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import ReactQueryProvider from "@/components/TanStackProvider/TanStackProvider";
+
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
-        <ReactQueryProvider>{children}</ReactQueryProvider>
-        <Footer />
+        {}
+        <TanStackProvider>
+          <Header />
+          {children}
+          <Footer />
+        </TanStackProvider>
       </body>
     </html>
   );
