add406cd77c4154b58292c7cd9824c0b716bc67b
Vadzim Simanau <eks220885@gmail.com>
2025-08-07 13:31:33 +0300

    fix problem with deploy and with warning in consol

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -8,7 +8,7 @@ export default async function NoteDetailsPage({
 }: {
   params: { id: string };
 }) {
-  const { id } = params;
+  const { id } = await params;
 
   const queryClient = new QueryClient();
 
