71f90ff945c04e82e138a9c0ba51a536f9769c98
Vadzim Simanau <eks220885@gmail.com>
2025-08-07 13:22:51 +0300

    fix deployment problem (params on [id]/page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -3,15 +3,11 @@ import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
 import { fetchNoteById } from "@/lib/api";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
-interface NoteDetailsPageProps {
-  params: {
-    id: string;
-  };
-}
-
 export default async function NoteDetailsPage({
   params,
-}: NoteDetailsPageProps) {
+}: {
+  params: { id: string };
+}) {
   const { id } = params;
 
   const queryClient = new QueryClient();
